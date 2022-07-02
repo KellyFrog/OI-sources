1a93d41e6806ae7f1c75e3b5f1d3fd5d34cf8a5e
陈凯丰 <chenkaifengkim@163.com>
2022-07-02 17:00:26 +0800

    20220702

---
+++ P5470.cpp
@@ -0,0 +1,33 @@
+// Problem: P5470 [NOI2019] 序列
+// Contest: Luogu
+// URL: https://www.luogu.com.cn/problem/P5470
+// Memory Limit: 512 MB
+// Time Limit: 2000 ms
+// Create Time: 2022-07-02 16:08:58
+// Author: Chen Kaifeng
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define rep(i, s, t) for (int i = s; i <= t; ++i)
+#define per(i, s, t) for (int i = t; i >= s; --i)
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	cout << fixed << setprecision(15); 
+	cerr << fixed << setprecision(15);
+	
+	return 0;
+}
