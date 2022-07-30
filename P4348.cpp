3ec3a636e65f634cc15347ef0b72cb0cb0612b12
ckf <chenkaifengkim@163.com>
2022-07-30 09:24:14 +0800

    fence

---
+++ P4348.cpp
@@ -0,0 +1,31 @@
+// Problem: P4348 [CERC2015]Cow Conﬁnement
+// Contest: Luogu
+// URL: https://www.luogu.com.cn/problem/P4348
+// Memory Limit: 512 MB
+// Time Limit: 3000 ms
+// Create Time: 2022-07-30 09:24:02
+// Input/Output: stdin/stdout
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define rep(i, s, t) for (int i = s; i <= t; ++i)
+#define per(i, s, t) for (int i = t; i >= s; --i)
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	cout << fixed << setprecision(15); 
+	cerr << fixed << setprecision(15);
+	
+	return 0;
+}
