44067741aed264d288fe7079202edca5cb964008
kellyfrog <chenkaifengkim@163.com>
2022-09-22 17:16:51 +0800

    20220922

--- AGC004F.cpp
+++ AGC004F.cpp
@@ -23,17 +23,34 @@ const int N = 1e5 + 5;
 
 int n, m;
 vector<int> adj[N];
-int uni[N];
+int uni[N], dep[N], cnt[N][2], s[N][2];
+ll ans = 0;
 int eu = 1, ev = 1;
 
 inline int find(int x) { return x == uni[x] ? x : uni[x] = find(uni[x]); }
+
+inline void dfs(int u, int fa) {
+	dep[u] = dep[fa] ^ 1;
+	cnt[u][dep[u]] = 1;
+	cnt[u][dep[u]^1] = 0;
+	cnt[u][0] += s[u][0];
+	cnt[u][1] += s[u][1];
+
+	for(int v : adj[u]) if(v != fa) {
+		dfs(v, u);
+		cnt[u][0] += cnt[v][0];
+		cnt[u][1] += cnt[v][1];
+	}
+	ans += abs(cnt[u][0] - cnt[u][1]);
+}
+
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr), cout.tie(nullptr);
 	cout << fixed << setprecision(15); 
 	cerr << fixed << setprecision(15);
-	
+
 	cin >> n >> m;
 	rep(i, 1, n) uni[i] = i;
 	rep(i, 1, m) {
@@ -44,8 +61,42 @@ int main() {
 		} else {
 			adj[u].pb(v);
 			adj[v].pb(u);
+			uni[x] = y;
 		}
 	}
+
+	dfs(1, 0);
+	if(n & 1) {
+		cout << -1 << "\n";
+		return 0;
+	}
+	if(dep[eu] == dep[ev]) {
+		ans = 0;
+
+		if(eu != ev) {
+			int x = abs(cnt[1][0] - cnt[1][1]) / 2;
+			if((cnt[1][0] < cnt[1][1]) ^ (dep[eu] & 1)) {
+				s[eu][dep[eu]] += x;
+				s[ev][dep[ev]] += x;
+			} else {
+				s[eu][dep[eu]^1] += x;
+				s[ev][dep[ev]^1] += x;
+			}
+			ans += x;
+		}
+
+		dfs(1, 0);
+
+		
+		if(cnt[1][0] != cnt[1][1]) {
+			cout << -1 << "\n";
+			return 0;
+		}
+		cout << ans << "\n";
+	} else {
+	}
+
+
 	
 	return 0;
 }
