3e70c9e3c5218b77c6392c8605e709c9f6d00dbb
chenkaifeng@vmware <chenkaifengkim@163.com>
2022-09-29 16:29:24 +0800

    20220929

--- CF148E.cpp
+++ CF148E.cpp
@@ -19,11 +19,35 @@ typedef long long ll;
 #define rep(i, s, t) for (int i = s; i <= t; ++i)
 #define per(i, s, t) for (int i = t; i >= s; --i)
 
+const int N = 105;
+const int M = 10005;
+
+int n, m;
+vector<int> a[N], s[N], val[N];
+int dp[M];
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr), cout.tie(nullptr);
 	cout << fixed << setprecision(15); 
 	cerr << fixed << setprecision(15);
 	
+	cin >> n >> m;
+	rep(i, 1, n) {
+		int k; cin >> k;
+		vector<int> a(k, 0), val(k+1, 0);
+		for(int& x : a) cin >> x;
+		vector<int> s = a, t = a;
+		reverse(t.begin(), t.end());
+		for(int j = 1; j < k; ++j) s[j] += s[j-1];
+		for(int j = 1; j < k; ++j) t[j] += t[j-1];
+		for(int j = 0; j <= k; ++j) {
+			for(int jj = -1; jj < j; ++jj) val[j] = max(val[j], (jj >= 0 ? s[jj] : 0) + (j-jj-2 >= 0 ? t[j-jj-2] : 0));
+		}
+		for(int j = 0; j <= k; ++j) cerr << val[j] << " \n"[j == k];
+		per(j, 1, m) rep(jj, 1, min(j, k)) dp[j] = max(dp[j], dp[j-jj] + val[jj]);
+	}
+	cout << dp[m] << "\n";
+
 	return 0;
 }
