b5e5a6ce29f9e9f81b8a2207c37a1eaa43a42ace
kellyfrog <chenkaifengkim@163.com>
2022-11-15 15:15:31 +0800

    20221115

---
+++ AGC006E/AGC006E.cpp
@@ -0,0 +1,29 @@
+// Problem: AT_agc006_e [AGC006E] Rotate 3x3
+// Contest: Luogu
+// URL: https://www.luogu.com.cn/problem/AT_agc006_e
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define rep(i, s, t) for (int i = s; i <= t; ++i)
+#define per(i, s, t) for (int i = t; i >= s; --i)
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	cout << fixed << setprecision(15); 
+	cerr << fixed << setprecision(15);
+	
+	return 0;
+}
