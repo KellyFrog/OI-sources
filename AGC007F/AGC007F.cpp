1c2939a5c371aa8ff7e23f9925b8005de6c4b4b9
chenkaifeng@vmware <chenkaifengkim@163.com>
2022-11-16 19:46:43 +0800

    20221116

--- AGC007F/AGC007F.cpp
+++ AGC007F/AGC007F.cpp
@@ -1,7 +1,7 @@
-// Problem: AT2173 [AGC007F] Shik and Copying String
+// Problem: AT_agc007_f [AGC007F] Shik and Copying String
 // Contest: Luogu
-// URL: https://www.luogu.com.cn/problem/AT2173
-// Memory Limit: 250 MB
+// URL: https://www.luogu.com.cn/problem/AT_agc007_f
+// Memory Limit: 256 MB
 // Time Limit: 2000 ms
 // 
 // Powered by CP Editor (https://cpeditor.org)
@@ -23,36 +23,32 @@ const int N = 1e6 + 5;
 
 int n;
 char s[N], t[N];
-int r[N], sum[N];
+int f[N], pos[N], lst[27];
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr), cout.tie(nullptr);
 	cout << fixed << setprecision(15); 
 	cerr << fixed << setprecision(15);
-	
+
 	cin >> n;
 	cin >> (s + 1) >> (t + 1);
+
 	int p = n;
 	per(i, 1, n) {
 		p = min(p, i);
 		while(p && s[p] != t[i]) --p;
-		if(!p) break;
-		r[p] = max(r[p], i);
+		if(!p) {
+			cout << -1 << "\n";
+			return 0;
+		}
+		pos[i] = p;
 	}
-	if(!p) {
-		cout << -1 << "\n";
-		return 0;
-	}
-	if(strcmp(s+1, t+1) == 0) {
-		cout << 0 << "\n";
-		return 0;
-	}
-	rep(i, 1, n) if(r[i]) ++sum[i], --sum[r[i]+1];
-	rep(i, 1, n) sum[i] += sum[i-1];
+	rep(i, 1, n) f[i] = pos[i] == i ? 0 : f[pos[i]] + 1;
 	int ans = 0;
-	rep(i, 1, n) ans = max(ans, sum[i]);
+	rep(i, 1, n) ans = max(ans, f[i]);
 	cout << ans << "\n";
 	
 	return 0;
 }
+
