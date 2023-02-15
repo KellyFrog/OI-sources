e27a6abc6dff12438b42071e87e602c34d609d80
chenkaifeng@vmware <chenkaifengkim@163.com>
2023-02-15 12:04:41 +0800

    20230215

--- P7413/P7413.cpp
+++ P7413/P7413.cpp
@@ -19,11 +19,35 @@ typedef long long ll;
 #define rep(i, s, t) for (int i = s; i <= t; ++i)
 #define per(i, s, t) for (int i = t; i >= s; --i)
 
+const int N = 1e6 + 5;
+
+int n, a[N], cnt[N], pre[N];
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr), cout.tie(nullptr);
 	cout << fixed << setprecision(15); 
 	cerr << fixed << setprecision(15);
+
+	cin >> n;
+	rep(i, 1, n) cin >> a[i], ++cnt[a[i]];
+	int m = 1e6;
+	rep(i, 1, m) pre[i] = pre[i-1] + cnt[i];
+	ll ans = 0;
+	rep(i, 1, m) {
+		auto getsum = [&](int l, int r) { return pre[min(r, m)] - pre[l-1]; };
+		int cnt = 0;
+		for(int j = i; j <= m; j += i) cnt += (getsum(j, i+j-1) & 1);
+		if(cnt == 0 || cnt > 2) continue;
+		if(cnt == 1) {
+			if(getsum(i, 2*i-1) & 1) ans += getsum(i, 2*i-1);
+		} else {
+			for(int j = i; j <= m; j += i) {
+				if((getsum(j, i+j-1) & 1) && (getsum(j-i, j-1) & 1)) ans += getsum(j, i+j-1);
+			}
+		}
+	}
+	cout << ans << "\n";
 	
 	return 0;
 }
--- P7528/P7528.cpp
+++ P7528/P7528.cpp
@@ -19,11 +19,43 @@ typedef long long ll;
 #define rep(i, s, t) for (int i = s; i <= t; ++i)
 #define per(i, s, t) for (int i = t; i >= s; --i)
 
+const int N = 4e5 + 5;
+
+int n;
+int uni[N], c[N], p[N][4], pe[N];
+vector<int> eg[N];
+
+inline int find(int x) { return x == uni[x] ? x : uni[x] = find(uni[x]); }
+inline void merge(int x, int y) { x = find(x), y = find(y); if(x != y) uni[x] = y; }
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr), cout.tie(nullptr);
 	cout << fixed << setprecision(15); 
 	cerr << fixed << setprecision(15);
+
+	cin >> n;
+	rep(i, 1, n) {
+		cin >> c[i] >> p[i][0] >> p[i][1] >> p[i][2] >> p[i][3];
+		eg[p[i][0]].pb(i), eg[p[i][1]].pb(i), eg[p[i][2]].pb(i+n), eg[p[i][3]].pb(i+n);
+	}
+	rep(i, 1, 2*n) assert(eg[i].size() == 2);
+	rep(i, 1, 2*n) uni[i] = i;
+	rep(i, 1, n) {
+		merge(i, eg[p[i][0]][0]), merge(i, eg[p[i][0]][1]);
+		merge(i, eg[p[i][1]][0]), merge(i, eg[p[i][1]][1]);
+		merge(i+n, eg[p[i][2]][0]), merge(i+n, eg[p[i][2]][1]);
+		merge(i+n, eg[p[i][3]][0]), merge(i+n, eg[p[i][3]][1]);
+	}
+	rep(i, 1, n) pe[i] = i;
+	sort(pe + 1, pe + n + 1, [&](int i, int j) { return c[i] < c[j]; });
+	int ans = 0;
+	rep(i, 1, n) {
+		int u = pe[i];
+		if(find(u) != find(u+n)) {
+			merge(u, u+n), ans += c[u];
+		}
+	}
+	cout << ans << "\n";
 	
 	return 0;
 }
