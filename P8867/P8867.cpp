f4ff86f15916e28e6c54081573781e81d1e32541
chenkaifeng@vmware <chenkaifengkim@163.com>
2023-02-22 13:03:30 +0800

    20230222

--- P8867/P8867.cpp
+++ P8867/P8867.cpp
@@ -19,11 +19,68 @@ typedef long long ll;
 #define rep(i, s, t) for (int i = s; i <= t; ++i)
 #define per(i, s, t) for (int i = t; i >= s; --i)
 
+const int N = 1e6 + 5;
+const int P = 1e9 + 7;
+const int inv2 = (P+1) / 2;
+
+int n, m;
+int head[N], nxt[N], to[N], tot = 1;
+int dfn[N], low[N], dfncnt, stk[N], top, bel[N], bc;
+vector<int> adj[N];
+int c1[N], c2[N];
+
+inline void addedge(int u, int v) {
+	nxt[++tot] = head[u];
+	to[tot] = v;
+	head[u] = tot;
+}
+
+inline void tarjan(int u, int lst) {
+	dfn[u] = low[u] = ++dfncnt;
+	stk[++top] = u;
+	for(int e = head[u]; e; e = nxt[e]) {
+		if(e == (lst ^ 1)) continue;
+		int v = to[e];
+		if(!dfn[v]) {
+			tarjan(v, e);
+			low[u] = min(low[u], low[v]);
+		} else {
+			low[u] = min(low[u], dfn[v]);
+		}
+	}
+	if(low[u] == dfn[v]) {
+		++bc;
+		while(1) {
+			int x = stk[top--];
+			bel[x] = bc;
+			if(x == u) break;
+		}
+	}
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr), cout.tie(nullptr);
 	cout << fixed << setprecision(15); 
 	cerr << fixed << setprecision(15);
+
+	cin >> n >> m;
+	rep(i, 1, m) {
+		int u, v; cin >> u >> v;
+		addedge(u, v), addedge(v, u);
+	}
+	tarjan(1, 0);
+	rep(i, 1, n) ++c1[bel[i]];
+	rep(u, 1, n) for(int e = head[u]; e; e = nxt[e]) {
+		int v = to[e];
+		if(bel[u] == bel[v]) ++c2[bel[u]];
+		else adj[bel[u]].pb(bel[v]), adj[bel[v]].pb(bel[u]);
+	}
+	rep(i, 1, bc) c2[i] >>= 1;
+	dfs(1, 0);
+	int ans = 0;
+	rep(i, 1, n) ans = (ans + f[i]) % P;
+	cout << ans << "\n";
 	
 	return 0;
 }
