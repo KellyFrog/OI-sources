cad21accbeac776abf93040dc65c67701962563d
kellyfrog <chenkaifengkim@163.com>
2023-03-11 17:00:45 +0800

    20230311

---
+++ codeforces/CF794G/CF794G.cpp
@@ -0,0 +1,39 @@
+// Problem: CF794G Replace All
+// Contest: Luogu
+// URL: https://www.luogu.com.cn/problem/CF794G
+// Memory Limit: 250 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define rep(i, s, t) for (int i = s; i <= t; ++i)
+#define per(i, s, t) for (int i = t; i >= s; --i)
+
+const int N = 3e5 + 5;
+
+int n, m, k;
+char s[N], t[N];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	cout << fixed << setprecision(15); 
+	cerr << fixed << setprecision(15);
+
+	cin >> (s+1) >> (t+1) >> k;
+	n = strlen(s+1), m = strlen(t+1);
+
+
+	
+	return 0;
+}
