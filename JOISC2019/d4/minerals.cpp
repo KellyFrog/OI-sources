ad05748e098d55f2ab3a6077063ef0c280f016c8
kellyfrog <chenkaifengkim@163.com>
2023-05-25 20:26:54 +0800

    20230525

--- JOISC2019/d4/minerals.cpp
+++ JOISC2019/d4/minerals.cpp
@@ -32,54 +32,18 @@ void solve(int al, int ar, int bl, int br) {
 		Answer(a[al], b[bl]);
 		return;
 	}
-	if(ar == al + 1) {
-		cerr << a[al] << " " << a[ar] << "\n";
-		cerr << b[bl] << " " << b[br] << "\n";
-		int t = las;
-		if(!vis[a[al]]) query(a[al]);
-		if(vis[b[bl]]) {
-			if(t == las) {
-				Answer(a[al], b[bl]);
-				Answer(a[ar], b[br]);
-			} else {
-				Answer(a[ar], b[bl]);
-				Answer(a[al], b[br]);
-			}
-		} else {
-			if(t == query(b[bl])) {
-				Answer(a[al], b[bl]);
-				Answer(a[ar], b[br]);
-			} else {
-				Answer(a[ar], b[bl]);
-				Answer(a[al], b[br]);
-			}
-		}
-		return;
-	}
 	int p = magic * ar + (1 - magic) * al;
-	if(vis[a[al]] == vis[b[bl]]) {
-		bool flag = vis[a[al]];
-		int t = 0;
-		rep(i, al, p) t = query(a[i]);
-		int p1 = bl-1, p2 = br+1;
-		rep(i, bl, br) {
-			int t0 = t;
-			if((t0 != (t = query(b[i])) ^ flag)) c[--p2] = b[i];
-			else c[++p1] = b[i];
-		}
-		rep(i, bl, br) b[i] = c[i];
-	} else {
-		if(!vis[a[al]]) rep(i, al, ar) swap(a[i], b[i-al+bl]);
-		int t = 0;
-		rep(i, al, p) t = query(a[i]);
-		int p1 = bl-1, p2 = br+1;
-		rep(i, bl, br) {
-			int t0 = t;
-			if(t0 == (t = query(b[i]))) c[--p2] = b[i];
-			else c[++p1] = b[i];
-		}
-		rep(i, bl, br) b[i] = c[i];
+	bool flag = vis[a[al]];
+	rep(i, al, p) query(a[i]);
+	int p1 = bl-1, p2 = br+1;
+	rep(i, bl, br) {
+		int t0 = las;
+		if(p1 == bl+p-al) c[--p2] = b[i];
+		else if(p2 == bl+p-al+1) c[++p1] = b[i];
+		else if((t0 != query(b[i]) ^ flag)) c[--p2] = b[i];
+		else c[++p1] = b[i];
 	}
+	rep(i, bl, br) b[i] = c[i];
 	solve(al, p, bl, bl+p-al);
 	solve(p+1, ar, bl+p-al+1, br);
 }
--- JOISC2019/d4/test.cpp
+++ JOISC2019/d4/test.cpp
@@ -13,7 +13,7 @@ typedef long long ll;
 
 const int N = 4.3e4 + 4;
 
-int f[N][3];
+int f[N];
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -24,19 +24,13 @@ int main() {
 	memset(f, 0x3f, sizeof f);
 
 	int n = 43000;
-	f[0][0] = f[0][1] = f[0][2] = 0;
-	f[1][0] = f[1][1] = f[1][2] = 0;
-	f[2][0] = f[2][1] = f[2][2] = 1;
+	f[0] = f[1] = 0;
+	f[2] = 2;
 	rep(i, 3, n) {
-		rep(j, 1, i-1) {
-			f[i][0] = min(f[i][0], i+j+f[j][2]+f[i-j][1]);
-			f[i][1] = min(f[i][1], i-j+i+f[j][2]+f[i-j][1]);
-			f[i][2] = min(f[i][2], i-j+i+f[j][1]+f[i-j][0]);
-		}
-		if(i <= 50) cerr << i << " " << f[i][0] << " " << f[i][1] << " " << f[i][2] << "\n";
+		rep(j, 1, i-1) f[i] = min(f[i], i + j + f[j] + f[i-j] - 1) ;
 	}
 
-	cout << min({f[n][0], f[n][1], f[n][2]}) + 2 * n << "\n";
+	cout << f[n] + 2 * n << "\n";
 
 	return 0;
 }
