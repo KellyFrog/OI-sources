2692742dff25e7af293cfe3e06fd4dfb5dabf29b
KellyFrog <chenkaifengkim@163.com>
2023-06-06 19:47:58 +0800

    20230606

---
+++ codeforces/CF505E/CF505E.cpp
@@ -0,0 +1,32 @@
+// Problem: Mr. Kitayuta vs. Bamboos
+// Contest: Luogu
+// URL: https://www.luogu.com.cn/problem/CF505E
+// Memory Limit: 250 MB
+// Time Limit: 2000 ms
+// Create Time: 2023-06-06 19:47:15
+// Input/Output: stdin/stdout
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define rep(i, s, t) for (int i = s; i <= t; ++i)
+#define per(i, s, t) for (int i = t; i >= s; --i)
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+	cout << fixed << setprecision(15); 
+	cerr << fixed << setprecision(15);
+	
+	return 0;
+}
+
