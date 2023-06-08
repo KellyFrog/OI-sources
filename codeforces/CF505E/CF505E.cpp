8ec71ce10a344271be698dfc477a918e929d66ed
KellyFrog <chenkaifengkim@163.com>
2023-06-08 14:38:46 +0800

    20230608

--- codeforces/CF505E/CF505E.cpp
+++ codeforces/CF505E/CF505E.cpp
@@ -21,11 +21,35 @@ typedef long long ll;
 #define rep(i, s, t) for (int i = s; i <= t; ++i)
 #define per(i, s, t) for (int i = t; i >= s; --i)
 
+const int N = 1e5 + 5;
+
+int n, m, k, p;
+int a[N], b[N];
+ll c[N], d[N];
+
+bool check(ll lim) {
+	rep(i, 1, n) d[i] = max(0ll, c[i] - lim);
+
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr), cout.tie(nullptr);
 	cout << fixed << setprecision(15); 
 	cerr << fixed << setprecision(15);
+
+	cin >> n >> m >> k >> p;
+	rep(i, 1, n) cin >> a[i] >> b[i], c[i] = a[i] + 1ll * (m-1) * b[i];
+
+	ll L = 0, R = 1e18;
+	ll res = -1;
+	while(L <= R) {
+		ll mid = L + R >> 1;
+		if(check(mid)) res = mid, R = mid - 1;
+		else L = mid + 1;
+	}
+
+	cout << res << "\n";
 	
 	return 0;
 }
