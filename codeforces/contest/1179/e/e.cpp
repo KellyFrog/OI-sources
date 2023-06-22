47b383a43412a8bf09e2c55b4b3f19f28dfd8144
ckf <chenkaifengkim@163.com>
2023-06-22 11:07:24 +0800

    20230622

--- codeforces/contest/1179/e/e.cpp
+++ codeforces/contest/1179/e/e.cpp
@@ -17,9 +17,10 @@ int n;
 ll L;
 ll ansl[N], ansr[N];
 mt19937 mtrnd(0x114514);
-map<pair<int, ll>, int> mem;
+map<pair<int, ll>, ll> mem;
 
 ll query(int i, ll x) {
+	if(x > (ll)1e18) return L;
 	if(mem.count(mp(i, x))) return mem[mp(i, x)];
 	cout << "? " << i << " " << x << endl;
 	ll ret; cin >> ret; return mem[mp(i, x)] = ret;
@@ -27,11 +28,12 @@ ll query(int i, ll x) {
 
 ll getpos(int i, ll y) {
 	ll x = 0;
-	per(j, 0, 4) if(query(i, x | 1ll << j) < y) x |= 1ll << j;
+	per(j, 0, 59) if(query(i, x | 1ll << j) < y) x |= 1ll << j;
 	return x + 1;
 }
 
 void solve(vector<int> v, int l, int r, ll ql, ll qr) {
+	assert(!v.empty());
 	if(v.size() == 1) {
 		int i = v[0];
 		ansl[i] = ql, ansr[i] = qr;
@@ -45,27 +47,31 @@ void solve(vector<int> v, int l, int r, ll ql, ll qr) {
 		int i = v[mtrnd() % v.size()];
 		ll x = getpos(i, y);
 		qmid = x;
-		vector<int> v1, v2;
+		vector<int> v1, v2, v3;
 		bool eq = 1;
 		for(int j : v) {
 			ll r = query(j, x);
-			if(r >= y) v1.pb(j);
+			if(r > y) v1.pb(j);
+			else if(r == y) v3.pb(j);
 			else v2.pb(j);
-			eq &= r == y;
 		}
-		if(eq) {
-			int d = mid-l+1 - vl.size();
-			vl.insert(vl.end(), v.begin(), v.begin() + d);
-			vr.insert(vr.end(), v.begin() + d, v.end());
-			assert(vl.size() + vr.size() == r-l+1);
+		v.clear();
+		if(vl.size() + v1.size() > mid-l+1) {
+			v = v1;
+			vr.insert(vr.end(), v2.begin(), v2.end());
+			vr.insert(vr.end(), v3.begin(), v3.end());
+		} else if(vr.size() + v2.size() > r-mid) {
+			v = v2;
+			vl.insert(vl.end(), v1.begin(), v1.end());
+			vl.insert(vl.end(), v3.begin(), v3.end());
 		} else {
-			v.clear();
-			if(vl.size() + v1.size() <= mid-l+1) vl.insert(vl.end(), v1.begin(), v1.end());
-			else v.insert(v.end(), v1.begin(), v1.end());
-			if(vr.size() + v2.size() <= r-mid) vr.insert(vr.end(), v2.begin(), v2.end());
-			else v.insert(v.end(), v2.begin(), v2.end());
+			vl.insert(vl.end(), v1.begin(), v1.end());
+			vr.insert(vr.end(), v2.begin(), v2.end());
+			int d = mid-l+1 - vl.size();
+			vl.insert(vl.end(), v3.begin(), v3.begin() + d);
+			vr.insert(vr.end(), v3.begin() + d, v3.end());
 		}
-	} while(vl.size() + vr.size() != r-l+1);
+	} while(!v.empty());
 	solve(vl, l, mid, ql, qmid);
 	solve(vr, mid+1, r, qmid, qr);
 }
@@ -76,9 +82,11 @@ int main() {
 	cout << fixed << setprecision(15); 
 	cerr << fixed << setprecision(15);
 
+	/*
 	int N, M;
 	cin >> N >> M;
 	rep(i, 1, N) rep(j, 1, M) cin >> mem[mp(i, j)];
+	*/
 
 	cin >> n >> L;
 	vector<int> v;
